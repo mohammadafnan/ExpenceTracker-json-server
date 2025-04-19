2a4639ce01b2cf1e2a5f64ff08be5bb0b70ebffb
Afnan <mohammad.afnan@millsoft.com.pk>
2025-04-19 13:59:30 +0500

    login component 21 april 2025

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,36 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  heading = 'Expense Tracker';
+  form: FormGroup;
 
-  login() {
-    this.router.navigate(['/expense']);
+  constructor(private router: Router, private formbulider: FormBuilder) {
+    this.form = this.formbulider.group({
+      loginname: ['', Validators.required],
+      loginpassword: ['', Validators.required],
+    });
+  }
+
+  login(data: any) {
+    if (data.loginname == 'afnan' && data.loginpassword == '12345') {
+      alert('Login Successfully');
+      this.router.navigate(['/expense']);
+    } else {
+      alert('Login failed');
+    }
   }
 }
