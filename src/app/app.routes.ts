42f2dc25d4220e79acaee0c8fa31b3a46c0b45cc
Afnan <mohammad.afnan@millsoft.com.pk>
2025-05-23 20:00:06 +0500

    header/sidebar component added  23 may 2025

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { ExpenseComponent } from './expense/expense.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
     { path: "", redirectTo: "login", pathMatch: "full" },
     { path: "login", component: LoginComponent },
     { path: "expense", component: ExpenseComponent },
+    { path: "dashboard", component: DashboardComponent },
+
 
 ];
 
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-sidebar',
   imports: [],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
+  constructor(public router: Router,public auth:AuthService) {}
 
+  gotodash() {
+    this.router.navigate(['/dashboard']);
+  }
+
+  gotoexpense() {
+    this.router.navigate(['/expense']);
+  }
+
+ 
 }
