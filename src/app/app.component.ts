11cf29f877c14608a5310ad6c385ec349c848864
Afnan <mohammad.afnan@millsoft.com.pk>
2025-05-27 18:31:21 +0500

    tailwind/flowbie install via npm 27 may 2025

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,31 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AfterViewInit, Component, ViewEncapsulation } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
-import { ExpenseComponent,  } from './expense/expense.component';
+import { ExpenseComponent } from './expense/expense.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+declare const initFlowbite: any;
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,SidebarComponent],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'pegasus_productWeb';
+  constructor(private router: Router) {}
+
+  ngAfterViewInit() {
+    this.router.events.subscribe((event) => {
+      if (
+        event instanceof NavigationEnd &&
+        typeof initFlowbite === 'function'
+      ) {
+        initFlowbite();
+      }
+    });
+  }
 }
